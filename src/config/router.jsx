4741e52f061ce944323e7761cd3b7501450beccb
mankinde23 <makinded12@gmail.com>
2023-11-07 07:55:45 +0000

    created a router component for the home page (home component)

---
+++ src/config/router.jsx
@@ -0,0 +1,12 @@
+import Home from "pages/home/home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Home />
+    }
+])
+const RouterComponent = () => {
+    <RouterProvider router={router} />
+}
+export default RouterComponent
