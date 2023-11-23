a96c2337058994bb1d2b96b1a9c7184ce5aeb250
mankinde23 <makinded12@gmail.com>
2023-11-23 06:29:20 +0000

    did a trialn and error action to solve a bug

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -3,15 +3,22 @@ import '../home/home.css'
 
 const Home = () => {
     return (
-        <div className='main-container'>
-            <div className='left-text'></div>
-            <div className='form-container'>
-                <div className='trial-container'></div>
-                <div className='form'></div>
-            </div>
+        <>
+            <div className='main-container'>
+                <div className='left-text'>
+                    <p>Learn to code by <br />
+                        watching others
+                    </p>
+                </div>
+                <div className='form-container'>
+                    <div className='trial-container'></div>
+                    <div className='form'>
+                        money
+                    </div>
+                </div>
 
-
-        </div>
+            </div >
+        </>
     )
 }
 
